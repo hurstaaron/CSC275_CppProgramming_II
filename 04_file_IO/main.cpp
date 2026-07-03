3b3edc54268365d2f3aa855738351c607319b786
Aaron Hurst <hurst.aaron1@gmail.com>
2026-07-03 05:52:53 -0700

    Adding assignment 4 w/ screenshots and notes.txt

--- 04_file_IO/main.cpp
+++ 04_file_IO/main.cpp
@@ -2,17 +2,13 @@
     Assignment 04: File I/O 
     Professor: Rawad Habib
     Start Date: 6/10/26
-    End Date:   6/24/26
+    End Date:   7/3/26
     By: Aaron Hurst
 */
 
-#include <iostream>   // needed so I can use cin and cout to talk to the user
-#include <fstream>    // needed so I can open, write to, and read from a text file
-#include <string>     // needed so I can use the string data type for text
-
-// I am not using "using namespace std" in this version
-// so every time I use something like cout or string I have to
-// write std:: in front of it to tell the compiler where it comes from
+#include <iostream>   
+#include <fstream>    
+#include <string>     
 
 int main()
 {
@@ -28,22 +24,22 @@ int main()
     std::cout << "When you are done typing notes, just type the word quit and press enter." << std::endl;
     std::cout << std::endl;
 
-    // this string will hold whatever the user types in on each line
+    // hold whatever the user types in on each line
     std::string userInput;
 
-    // this string is just the name of the file we are saving everything to
-    // I made it a variable instead of typing "notes.txt" everywhere in the code
-    // in case I ever want to change the file name later, only have to change it once
+    // name of the file we are saving everything to
+    // variable instead of typing "notes.txt" everywhere in the code
+    // in case I ever want to change the file name later, I only need to change it once
     std::string fileName = "notes.txt";
 
-    // this keeps track of how many notes the user has typed in, starts at zero
+    // track of how many notes the user has typed in, starts at zero
     int noteCount = 0;
 
-    // ofstream is the data type used for writing OUT to a file
+    // ofstream is the data type used for writing OUT to a file,
     // opening it here so the file is ready to be written to
     std::ofstream outputFile(fileName);
 
-    // if the file did not open for some reason, tell the user and end the program
+    // if the file did not open, tell the user and end the program
     // instead of letting it crash or do something weird
     if (!outputFile)
     {
@@ -51,8 +47,7 @@ int main()
         return 0;
     }
 
-    // this loop keeps asking the user for a note over and over again
-    // it will keep looping forever until the user types the word quit
+    // while loop to keep asking the user for a note they type "quit
     while (true)
     {
         std::cout << "Enter a note (or type quit to stop): ";
@@ -64,29 +59,29 @@ int main()
             break;
         }
 
-        // this line writes the note the user typed into the text file
+        // writes the note the user typed into the text file
         outputFile << userInput << std::endl;
 
-        // this line prints that exact same note to the screen right away
-        // so the file and the screen are getting the same info at the same time
+        // prints that note to the screen right away so the file 
+        // and the screen are getting the same info at the same time
         std::cout << "Saved to file: " << userInput << std::endl;
 
-        // add one to the note counter since we just saved a note
+        // add to the note counter
         noteCount++;
     }
 
-    // now that the loop is over, close the output file so everything actually saves
+    // end the loop and close the output file so everything aves
     outputFile.close();
 
     std::cout << std::endl;
     std::cout << "You entered " << noteCount << " notes. They have been saved to " << fileName << std::endl;
     std::cout << std::endl;
 
-    // now we open the file again, but this time we are reading from it instead of writing
+    // pen the file again to read it instead of writing
     // ifstream is the data type used for reading IN from a file
     std::ifstream inputFile(fileName);
 
-    // if the file did not open for reading, tell the user and end the program
+    // if the file did not open tell the user and end the program
     if (!inputFile)
     {
         std::cout << "Could not open the file to read it back. Closing program." << std::endl;
@@ -96,10 +91,10 @@ int main()
     std::cout << "Here is everything that is saved in " << fileName << ":" << std::endl;
     std::cout << "-------------------------------------" << std::endl;
 
-    // this string will hold each line as we pull it out of the file one at a time
+    // hold each line as we pull it out of the file one at a time
     std::string lineFromFile;
 
-    // this loop reads the file line by line until there is nothing left to read
+    // loop reads the file line by line until there is nothing left to read
     // getline returns false once it hits the end of the file, which stops the loop
     while (std::getline(inputFile, lineFromFile))
     {
@@ -109,7 +104,7 @@ int main()
 
     std::cout << "-------------------------------------" << std::endl;
 
-    // close the input file since we are done reading from it
+    // close the input file
     inputFile.close();
 
     std::cout << std::endl;
